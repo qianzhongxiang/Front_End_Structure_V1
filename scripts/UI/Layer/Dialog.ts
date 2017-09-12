a6277e6cbfa50af92f478b86203c4facf449348a
qianzhongxiang <qianzhongxiang@outlook.com>
2017-09-12 23:11:02 +0800

    during developing

---
+++ scripts/UI/Layer/Dialog.ts
@@ -0,0 +1,8 @@
+/// <reference path="../Widget.ts"/>
+namespace UI.Layer{
+    export class  Dialog extends Widget
+    {
+        protected defaultOptions
+        
+    }
+}
--- scripts/UI/Layer/ModalLayer.ts
+++ scripts/UI/Layer/ModalLayer.ts
@@ -1,9 +1,10 @@
 /// <reference path="../Widget.ts"/>
 namespace UI.Layer{
     export class ModalLayer extends Widget {
-        constructor(element?:HTMLElement,options?){
+        constructor(element?:HTMLDivElement,options?){
             let ele=element||document.createElement("div");
-            ele.style.height="100%";
+            ele.classList.add("modal");
+            ele.classList.add("fade");
             super(ele,options);
         }
     }
