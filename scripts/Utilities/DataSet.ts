90379088ccefa5f719310f61ee89024aa8e17005
钱忠祥Channe.Qian <qianzhongxiang@outlook.com>
2018-01-05 17:22:49 +0800

    2018/1/5

--- scripts/Utilities/DataSet.ts
+++ scripts/Utilities/DataSet.ts
@@ -1,3 +1,6 @@
+import { Ajax } from "./Ajax";
+import { Observerable, IObseverable } from "../Patterns/Observerable";
+import { Singleton } from "../Patterns/Singleton";
 export abstract class Cookie {
     static Set(c_name, value, expiredays) {
         var exdate = new Date()
@@ -18,3 +21,48 @@ export abstract class Cookie {
         return ""
     }
 }
+
+export let GetValue = (obj: Object, path: string) => {
+    if (!obj) return null;
+    let array = path.trim().split('.'), value: Object = obj
+    for (var i = 0; i < array.length; i++) {
+        if (!value) return null;
+        value = value[array[i]];
+    }
+    return value;
+};
+
+export interface IConfigManager {
+    /**field: name? {string} 支持路径形式：obj.Name 不是拷贝内容 小心修改*/
+    GetConfig(name?: string)
+}
+/**
+ * ConfigManager 请使用 单例模式  {IConfigManager}
+ */
+export class ConfigManager implements IConfigManager {
+    private config: Object
+    private Deserialize(): void {
+        new Ajax({ url: "/config.json", async: false, method: "GET" }).done((d) => {
+            this.config = d;//JSON.parse(d);
+        });
+    }
+    public GetConfig(name?: string): Object {
+        if (!this.config) this.Deserialize();
+        return name ? GetValue(this.config, name) : this.config;
+    }
+}
+
+export interface IUserSetings extends IObseverable {
+
+
+}
+
+/**
+ * UserSettings 请使用 单例模式 {IUserSetings}
+ */
+export class UserSettings extends Observerable implements IUserSetings{
+    
+}
+
+export let GetConfigManager: () => IConfigManager = Singleton(ConfigManager, true)
+export let GetUserSettings: () => IUserSetings = Singleton(UserSettings, true)
