9d32125e72bb712c14b551bc5acf8fcd70f94aa2
钱忠祥Channe.Qian <qianzhongxiang@outlook.com>
2018-02-11 13:48:07 +0800

    ii

--- Index.ts
+++ Index.ts
@@ -1,4 +1,4 @@
-export { VinciSearcher } from './scripts/UI/Editor/VinciSearcher';
+export { VinciSearcher,IVinciSearcherOptions } from './scripts/UI/Editor/VinciSearcher';
 export { Singleton } from './scripts/Patterns/Singleton';
 export { LinkList } from './scripts/DataStructures/LinkList';
 export { VinciWindow, IVinciWindowOptions } from './scripts/UI/Layer/VinciWindow';
