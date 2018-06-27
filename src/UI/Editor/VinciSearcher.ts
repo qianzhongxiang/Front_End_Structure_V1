35ea15c187e188e14ae39f0b3dc48f01333c5018
钱忠祥Channe.Qian <qianzhongxiang@outlook.com>
2018-06-27 09:41:31 +0800

    update

--- src/UI/Editor/VinciSearcher.ts
+++ src/UI/Editor/VinciSearcher.ts
@@ -4,7 +4,7 @@ import { DataSource } from './../../Utilities/DataSource';
 import { VinciEditorBase, IVinciEditorBaseEvents } from "./VinciEditorBase";
 import { GetValue } from '../../Utilities/DataSet';
 import { Extend } from '../../Utilities/Extend';
-export interface IVinciSearcherEvents extends IVinciEditorBaseEvents{
+export interface IVinciSearcherEvents extends IVinciEditorBaseEvents {
     Opened: string
     Closed: string
 }
@@ -16,11 +16,11 @@ export interface IVinciSearcherOptions {
 }
 //TODO inheriting vinciinput in the futrue
 export class VinciSearcher extends VinciEditorBase<IVinciSearcherOptions>{
-    public Events:IVinciSearcherEvents=Extend( { Opened: 'opened', Closed: 'closed' }, this.Events)
+    public Events: IVinciSearcherEvents = Extend({ Opened: 'opened', Closed: 'closed' }, this.Events)
     private Table: VinciTable
     private Window: VinciWindow
     public get DefaultOptions(): IVinciSearcherOptions {
-        return { ValueField: "value", TextField: "text" ,Columns:[]}
+        return { ValueField: "value", TextField: "text", Columns: [] }
     }
     constructor(element: HTMLInputElement, options?: IVinciSearcherOptions) {
         super(element, options);
@@ -28,25 +28,25 @@ export class VinciSearcher extends VinciEditorBase<IVinciSearcherOptions>{
     protected Initialization() {
         if (this.Element == this.Wrapper) {
             this.Element.parentElement.insertBefore(this.Wrapper = document.createElement("div"), this.Element);
-            let element=this.Element as HTMLInputElement;
+            let element = this.Element as HTMLInputElement;
             element.placeholder = "Search for...";
-            element.type="search";
+            element.type = "search";
             element.classList.add("form-control")
-            element.addEventListener("change",(e)=>{
-                if(!(e.target as HTMLInputElement).value)
-                this.SetValue(undefined);
+            element.addEventListener("change", (e) => {
+                if (!(e.target as HTMLInputElement).value)
+                    this.SetValue(undefined);
             })
             this.Wrapper.appendChild(this.Element);
             this.Wrapper.classList.add("input-group");
             let appendDiv = document.createElement("div"),
                 btn = document.createElement("button");
-                appendDiv.classList.add("input-group-append")
+            appendDiv.classList.add("input-group-append")
             btn.classList.add("btn", 'btn-outline-secondary');//,'fa','fa-search'
-            btn.innerHTML="<i class='fa fa-search'></i>&nbsp;"
+            btn.innerHTML = "<i class='fa fa-search'></i>&nbsp;"
             // btn.innerText="GO";
             btn.type = "button";
-            btn.addEventListener("click",()=>{
-                if(!this.Window||!this.Window.Opened)this.Open()
+            btn.addEventListener("click", () => {
+                if (!this.Window || !this.Window.Opened) this.Open()
                 else this.Close();
             })
             appendDiv.appendChild(btn)
@@ -55,10 +55,10 @@ export class VinciSearcher extends VinciEditorBase<IVinciSearcherOptions>{
     }
     public Open() {
         let tDiv = document.createElement("div");
-        tDiv.style.maxHeight="450px";
-        tDiv.style.overflow="auto";
-        this.Table = new VinciTable(tDiv, { Columns: this.Options.Columns, DataSource: this.Options.DataSource ,Dbclickable:true});
-        this.Table.Bind(this.Table.Events.OnDblclick, (msg) => {
+        tDiv.style.maxHeight = "450px";
+        tDiv.style.overflow = "auto";
+        this.Table = new VinciTable(tDiv, { Columns: this.Options.Columns, DataSource: this.Options.DataSource });
+        this.Table.Bind(this.Table.Events.OnSelect, (msg) => {
             if (msg.Value) {
                 (this.Element as HTMLInputElement).value = GetValue(msg.Value, this.Options.TextField).toString();
                 this.SetValue(GetValue(msg.Value, this.Options.ValueField));
--- src/UI/Layer/VinciWindow.ts
+++ src/UI/Layer/VinciWindow.ts
@@ -1,9 +1,12 @@
 import { VinciLayerBase } from './VinciLayerBase';
 import { VinciModalLayer } from "./VinciModalLayer";
 
-export interface IVinciWindowOptions  {
+export interface IVinciWindowOptions {
     AutoDestory?: boolean
     Title?: string
+    Width?: string
+    Height?: string
+    Size?: 'lg' | 'sm' | 'unlimited'
     // Align?:string
 }
 export class VinciWindow extends VinciLayerBase<IVinciWindowOptions> {
@@ -22,20 +25,34 @@ export class VinciWindow extends VinciLayerBase<IVinciWindowOptions> {
             let c = this.Remove(this.GetChild(this.MODELLAYER));
             if (c) c.Destroy();
         }
-        this.MODELLAYER = this.Add(new VinciModalLayer(undefined, { opacity:0.4 })).Id;
+        this.MODELLAYER = this.Add(new VinciModalLayer(undefined, { opacity: 0.4 })).Id;
 
         if (this.Wrapper.parentNode)
             this.Wrapper.parentNode.removeChild(this.Wrapper)
         this.Wrapper = document.createElement("div");
-        this.Wrapper.style.color="black";
+        this.Wrapper.style.color = "black";
         let dialog = document.createElement("div");
         dialog.classList.add("modal-dialog")
+        switch (this.Options.Size) {
+            case "lg":
+                dialog.classList.add("modal-lg");
+                break;
+            case "sm":
+                dialog.classList.add("modal-sm");
+                break;
+            case "unlimited":
+                dialog.style.maxWidth = "none";
+                break;
+        }
+        dialog.style.width = this.Options.Width
+        dialog.style.height = this.Options.Height
         let content = document.createElement("div");
+        content.style.height = "100%"
         dialog.appendChild(content);
         content.classList.add("modal-content")
         content.appendChild(this.GenerateHeader());
         content.appendChild(this.GenerateContent());
-        content.appendChild(this.GenerateFoot());
+        // content.appendChild(this.GenerateFoot());
         this.Wrapper.appendChild(dialog);
         this.Wrapper.classList.add("center-block"); //default Align:"center"
         this.Wrapper.classList.add("modal"); //animation need fade
@@ -44,6 +61,7 @@ export class VinciWindow extends VinciLayerBase<IVinciWindowOptions> {
     private GenerateHeader(): HTMLDivElement {
         let header = document.createElement("div");
         header.classList.add("modal-header");
+        header.style.padding = "8px 1em";
         let h4 = document.createElement("h4");
         h4.classList.add("modal-title")
         h4.innerText = this.Options.Title;
